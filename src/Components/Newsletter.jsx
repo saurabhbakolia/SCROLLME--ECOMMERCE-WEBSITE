2a63c30281465e693e5b3eb5a491baf6c2f33b6e
saurabh bakolia <skbakolia2002@gmail.com>
2023-02-18 20:15:46 +0530

    Newsletter done

--- src/Components/Newsletter.jsx
+++ src/Components/Newsletter.jsx
@@ -1,5 +1,7 @@
 import { Send } from "@mui/icons-material";
 import styled from "styled-components"
+import {mobile} from '../responsive'
+
 
 const Container = styled.div`
     height:60vh;
@@ -19,6 +21,7 @@ const Description = styled.div`
     font-size: 24px;
     font-weight: 300;
     margin-bottom: 20px;
+    ${mobile({ textAlign: "center" })}
 `;
 const InputContainer = styled.div`
     width: 50%;
@@ -27,6 +30,7 @@ const InputContainer = styled.div`
     display: flex;
     justify-content: space-between;
     border: 1px solid lightgray;
+    ${mobile({ width: "80%" })}
 `;
 const Input = styled.input`
     border: none;
