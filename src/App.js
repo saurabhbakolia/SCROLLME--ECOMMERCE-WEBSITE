e299fd60e4e6c65a8929f82a177684924a5d7183
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2023-11-03 19:34:15 +0530

    small changes in index.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="product" element={<Product />} />
-            <Route path="productList" element={<ProductList />} />
-            <Route path="cart" element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="product" element={<Product />} />
+        <Route path="productList" element={<ProductList />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
