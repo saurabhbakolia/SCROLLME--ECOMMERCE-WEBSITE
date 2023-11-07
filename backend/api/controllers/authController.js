ce4f1bd0687b757d8fe9e1b655643cc1bb6d2284
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2023-11-07 17:13:58 +0530

    auth Controller but fixed

--- backend/api/controllers/authController.js
+++ backend/api/controllers/authController.js
@@ -3,14 +3,63 @@
 const jwt = require("jsonwebtoken");
 const UserToken = require("../models/userToken");
 const verifyAccessToken = require("../utils/verifyAccessToken");
+const verifyRefreshToken = require("../utils/verifyRefreshToken");
 
 exports.checkAuthStatus = async (req, res) => {
     try {
         // Assuming the token is sent as an HTTP-only cookie named 'accessToken'
         const accessToken = req.cookies.accessToken;
+        const refreshToken = req.cookies.refreshToken;
 
         // If there's no token, the user is not logged in
-        if (!accessToken) {
+        if (!accessToken && refreshToken) {
+            verifyRefreshToken(refreshToken)
+                .then(async ({ user }) => {
+                    if (!user) {
+                        return res.status(401).json({
+                            status: 'fail',
+                            message: 'You are not logged in!',
+                            data: {
+                                isAuthenticated: false
+                            }
+                        });
+                    };
+                    const payload = { _id: user._id, roles: user.roles };
+                    const accessToken = jwt.sign(
+                        payload,
+                        'SCROLLME_SECRET',
+                        { expiresIn: '15m' }
+                    );
+                    await UserToken.findOneAndUpdate(
+                        { userId: user._id },
+                        { accessToken: accessToken },
+                    );
+                    res.cookie('accessToken', accessToken, {
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: 'none',
+                        maxAge: 15 * 60 * 1000, // token expiration time in milliseconds
+                    });
+                    res.status(200).json({
+                        status: 'success',
+                        message: 'New access token generated successfully!',
+                        data: {
+                            isAuthenticated: true,
+                            user: user
+                        }
+                    });
+                })
+                .catch(err => {
+                    console.log("Error signing access token");
+                    return res.status(401).json({
+                        status: 'fail',
+                        message: 'You are not logged in!',
+                        data: {
+                            isAuthenticated: false
+                        }
+                    });
+                })
+        } else if (!accessToken && !refreshToken) {
             return res.status(401).json({
                 status: 'fail',
                 message: 'You are not logged in!',
@@ -42,7 +91,7 @@ exports.checkAuthStatus = async (req, res) => {
                 });
             })
             .catch((err) => {
-                console.log("Error in finding user token:", err);
+                console.log("Error verifying access token");
                 return res.status(401).json({
                     status: 'fail',
                     message: 'You are not logged in!',
