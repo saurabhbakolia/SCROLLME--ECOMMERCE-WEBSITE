adf71495d207a7aabd7fa592e5a674900a9028ae
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2023-12-20 06:37:00 +0530

    Announcement path changed to ./../components/{}

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Announcement from '../components/Announcement'
+import Announcement from './../components/Announcement'
 import Categories from '../components/Categories'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
