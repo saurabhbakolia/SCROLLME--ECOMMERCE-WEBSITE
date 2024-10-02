e1ef78bf69d8fdee0dd92376750eb1d9e9bc5da9
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2024-10-02 18:36:29 +0530

    Features Implemented

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,33 +1,32 @@
-import styled from "styled-components"
+
+import styled from "styled-components";
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import { slideItems } from "../data";
-import { useState } from "react";
-import { mobile, tablet } from "../responsive"
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { mobile, tablet } from "../responsive";
 import OutlinedButton from "./OutlinedButton";
 import LazyLoad from 'react-lazyload';
 
-
 const Container = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 70vh; 
     display: flex;
     position: relative;
     overflow: hidden;
-    ${mobile({ display: "none;" })}
+    ${mobile({ height: "55vh" })} 
 `;
 
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
     background-color: #fff7f7;
-    border-radius:50%;
+    border-radius: 50%;
     display: flex;
     align-items: center;
     justify-content: center;
     position: absolute;
-    top:0;
-    bottom:0;
+    top: 0;
+    bottom: 0;
     left: ${props => props.direction === "left" && "10px"};
     right: ${props => props.direction === "right" && "10px"};
     margin: auto;
@@ -44,35 +43,51 @@ const Wrapper = styled.div`
 `;
 
 const Slide = styled.div`
-    width:100vw;
-    height: 100vh;
+    width: 100vw;
+    height: 100%; 
     display: flex;
     align-items: center;
     background-color: ${(props) => props.bg};
-    // background-color:#f5fafd;
+    position: relative; 
+    ${tablet({ flexDirection: "column" })} 
 `;
+
 const ImgContainer = styled.div`
-    height:100%;
-    flex:1;
+    height: 100%;
+    flex: 1;
+    position: relative;
 `;
+
 const Image = styled.img`
-    height:60%;
+    height: 90%;
+    width: 100%;
+    object-fit: cover;
 `;
+
 const InfoContainer = styled.div`
-    flex:1;
-    padding:50px;
+    flex: 1;
+    padding: 20px; 
+    display: flex; 
+    flex-direction: column; 
+    justify-content: center; 
     text-align: left;
-    ${tablet({ display: "none" })}
+    ${tablet({ padding: "10px", textAlign: "center", justifyContent: "flex-start" })} 
+    ${mobile({ padding: "5px", justifyContent: "flex-start" })} 
 `;
 
 const Title = styled.h1`
-    font-size: 70px;
+    font-size: 50px; 
+    ${tablet({ fontSize: "40px" })} 
+    ${mobile({ fontSize: "25px" })} 
 `;
+
 const Desc = styled.p`
-    margin: 50px 0px;
-    font-size: 20px;
+    margin: 20px 0px;
+    font-size: 16px; 
     font-weight: 500;
-    letter-spacing:3px; 
+    letter-spacing: 2px; 
+    ${tablet({ fontSize: "14px", margin: "10px 0" })} 
+    ${mobile({ fontSize: "12px", margin: "5px 0" })} 
 `;
 
 const Placeholder = styled.div`
@@ -81,13 +96,35 @@ const Placeholder = styled.div`
     background-color: #f0f0f0;
 `;
 
+const ButtonContainer = styled.div`
+    position: relative; /* Default position */
+    margin-top: 0; 
+    
+
+    /* Position absolutely for small devices */
+    ${mobile({ 
+        position: "absolute", 
+        top: "20px", 
+        right: "20px",
+    })} 
+`;
+
 const Slider = () => {
     const [slideIndex, setSlideIndex] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSlideIndex(prev => (prev < slideItems.length - 1 ? prev + 1 : 0));
+        }, 3000); 
+
+        return () => clearInterval(timer);
+    }, []);
+
     const handleClick = (direction) => {
         if (direction === "left") {
-            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : slideItems.length - 1);
         } else {
-            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+            setSlideIndex(slideIndex < slideItems.length - 1 ? slideIndex + 1 : 0);
         }
     };
 
@@ -107,7 +144,9 @@ const Slider = () => {
                         <InfoContainer>
                             <Title>{item.title}</Title>
                             <Desc>{item.desc}</Desc>
-                            <OutlinedButton text={"SHOP NOW"} link={"/products"} />
+                            <ButtonContainer>
+                                <OutlinedButton text={"SHOP NOW"} link={"/products"} />
+                            </ButtonContainer>
                         </InfoContainer>
                     </Slide>
                 ))}
@@ -116,7 +155,7 @@ const Slider = () => {
                 <ArrowRightOutlined />
             </Arrow>
         </Container>
-    )
+    );
 }
 
-export default Slider
+export default Slider;
