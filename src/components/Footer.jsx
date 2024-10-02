7a1a98c9b42c939e11877f6251acf6e879c87789
Rizvi-Faiz <rizvifaiz1073@gmail.com>
2024-10-02 09:28:55 +0530

    Improve footer design by adding content, modernizing design, and ensuring accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,25 @@
 import { Email, Facebook, Instagram, LinkedIn, LocalPhone, LocationOn, Twitter } from "@mui/icons-material";
-import styled from "styled-components"
-import { mobile } from '../responsive'
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     display: flex;
-    ${mobile({ flexDirection: "column" })}
+    flex-wrap: wrap;
+    background-color: #333;
+    color: white;
+    padding: 20px;
+    ${mobile({ flexDirection: "column" })};
 `;
 
 const Left = styled.div`
     flex: 1;
-    display: flex;
-    flex-direction: column;
     padding: 20px;
 `;
 
-const Logo = styled.h1``;
+const Logo = styled.h1`
+    color: teal;
+`;
 
 const Desc = styled.p`
     margin: 20px 0px;
@@ -24,10 +27,10 @@ const Desc = styled.p`
 
 const SocialContainer = styled.div`
     display: flex;
-    ${mobile({ justifyContent: "center" })}
+    margin-top: 10px;
 `;
 
-const SocialIcon = styled.div`
+const SocialIcon = styled.a`
     width: 40px;
     height: 40px;
     border-radius: 50%;
@@ -37,6 +40,11 @@ const SocialIcon = styled.div`
     align-items: center;
     justify-content: center;
     margin-right: 20px;
+    cursor: pointer;
+    text-decoration: none;
+    &:hover {
+        opacity: 0.8;
+    }
 `;
 
 const Center = styled.div`
@@ -45,7 +53,7 @@ const Center = styled.div`
 `;
 
 const Title = styled.h3`
-  margin-bottom: 30px;
+    margin-bottom: 20px;
 `;
 
 const List = styled.ul`
@@ -61,23 +69,28 @@ const ListItem = styled.li`
     margin-bottom: 10px;
 `;
 
+const LinkStyled = styled(Link)`
+    text-decoration: none;
+    color: white;
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
 const Right = styled.div`
     flex: 1;
     padding: 20px;
-    ${mobile({ backgroundColor: "#fff8f8" })}
 `;
 
 const ContactItem = styled.div`
     margin-bottom: 20px;
     display: flex;
     align-items: center;
-    ${mobile({ justifyContent: "center" })}
 `;
 
 const Payment = styled.img`
     width: 50%;
 `;
-
 
 const Footer = () => {
     return (
@@ -85,19 +98,19 @@ const Footer = () => {
             <Left>
                 <Logo>SCROLL<span style={{ color: 'teal' }}>ME</span>.</Logo>
                 <Desc>
-                    Welcome to <span style={{ color: 'teal', fontWeight: 600 }}>ScrollMe</span> Web Store, your ultimate destination for trendy and fashionable clothing. We pride ourselves on curating a diverse collection of high-quality apparel that caters to your unique style and personality.
+                    Welcome to <span style={{ color: 'teal', fontWeight: 600 }}>ScrollMe</span> Web Store, your ultimate destination for trendy clothing.
                 </Desc>
                 <SocialContainer>
-                    <SocialIcon color="3B5999">
+                    <SocialIcon href="https://facebook.com" color="3B5999" aria-label="Facebook">
                         <Facebook />
                     </SocialIcon>
-                    <SocialIcon color="E4405F">
+                    <SocialIcon href="https://instagram.com" color="E4405F" aria-label="Instagram">
                         <Instagram />
                     </SocialIcon>
-                    <SocialIcon color="55ACEE">
+                    <SocialIcon href="https://twitter.com" color="55ACEE" aria-label="Twitter">
                         <Twitter />
                     </SocialIcon>
-                    <SocialIcon color="E60023">
+                    <SocialIcon href="https://linkedin.com" color="E60023" aria-label="LinkedIn">
                         <LinkedIn />
                     </SocialIcon>
                 </SocialContainer>
@@ -105,33 +118,28 @@ const Footer = () => {
             <Center>
                 <Title>Useful Links</Title>
                 <List>
-                    <ListItem><Link to={'/'}>Home</Link></ListItem>
-                    <ListItem><Link to={'/cart'}>Cart</Link></ListItem>
-                    <ListItem>Man Fashion</ListItem>
-                    <ListItem>Woman Fashion</ListItem>
-                    <ListItem>Accessories</ListItem>
-                    <ListItem>My Account</ListItem>
-                    <ListItem>Order Tracking</ListItem>
-                    <ListItem>Wishlist</ListItem>
-                    <ListItem>Wishlist</ListItem>
-                    <ListItem>Terms</ListItem>
+                    <ListItem><LinkStyled to="/">Home</LinkStyled></ListItem>
+                    <ListItem><LinkStyled to="/cart">Cart</LinkStyled></ListItem>
+                    <ListItem><LinkStyled to="/privacy-policy">Privacy Policy</LinkStyled></ListItem>
+                    <ListItem><LinkStyled to="/terms">Terms of Service</LinkStyled></ListItem>
+                    <ListItem><LinkStyled to="/contact">Contact Us</LinkStyled></ListItem>
                 </List>
             </Center>
             <Right>
                 <Title>Contact</Title>
                 <ContactItem>
-                    <LocationOn style={{ marginRight: "10px" }} /> 622 Dixie Path , South Tobinchester 98336
+                    <LocationOn style={{ marginRight: "10px" }} /> 622 Dixie Path, South Tobinchester 98336
                 </ContactItem>
                 <ContactItem>
                     <LocalPhone style={{ marginRight: "10px" }} /> +1 234 56 78
                 </ContactItem>
                 <ContactItem>
-                    <Email style={{ marginRight: "10px" }} /> contact@lama.dev
+                    <Email style={{ marginRight: "10px" }} /> contact@scrollme.dev
                 </ContactItem>
-                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" alt="Payment Methods" />
             </Right>
         </Container>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
