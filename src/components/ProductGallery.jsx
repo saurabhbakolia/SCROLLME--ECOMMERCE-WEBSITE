72d7a268d2a531f5a131a7b6578d661c1721a441
tarunkumar2005 <tarunkumar6258278@gmail.com>
2024-10-06 11:07:14 +0530

    Added the filtering of products based on category #42 Seperating products based on category.

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './ProductCard';
-import { allProducts } from '../data';
+import { allProducts, productCategories } from '../data';
 import styled from 'styled-components';
-
 
 const ProductGalleryContainer = styled.div`
     display: grid;
@@ -21,21 +20,64 @@ const ProductGalleryContainer = styled.div`
     @media only screen and (max-width: 480px) {
         grid-template-columns: repeat(1, 1fr);
     }
-
 `;
 
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  margin-top: 70px;
+`;
+
+const FilterButton = styled.button`
+  padding: 10px 20px;
+  margin: 0 10px;
+  background-color: ${(props) => (props.active ? '#3498db' : '#f0f0f0')};
+  color: ${(props) => (props.active ? '#fff' : '#000')};
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #3498db;
+    color: white;
+  }
+`;
 
 const ProductGallery = () => {
-    return (
-        <React.Fragment>
-            <h2>Products Gallery</h2>
-            <ProductGalleryContainer>
-                {allProducts.map((product) => (
-                    <ProductCard product={product} key={product.id} />
-                ))}
-            </ProductGalleryContainer>
-        </React.Fragment>
-    )
-}
+  // State to track the selected category
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
-export default ProductGallery
+  // Categories for filtering
+  const categories = ['All', ...productCategories];
+
+  // Filter products based on the selected category
+  const filteredProducts =
+    selectedCategory === 'All'
+      ? allProducts
+      : allProducts.filter((product) => product.category === selectedCategory);
+
+  return (
+    <React.Fragment>
+      <h2>Products Gallery</h2>
+      {/* Filter Buttons */}
+      <FilterContainer>
+        {categories.map((category) => (
+          <FilterButton
+            key={category}
+            active={selectedCategory === category}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </FilterButton>
+        ))}
+      </FilterContainer>
+      {/* Product Cards */}
+      <ProductGalleryContainer>
+        {filteredProducts.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </ProductGalleryContainer>
+    </React.Fragment>
+  );
+};
+
+export default ProductGallery;
