6acb11675ae906a3661a4ce1b77348b9ee1a0d11
vikas83pal <vikas83pal@gmail.com>
2024-10-06 13:16:18 +0530

    Update Categories and Product components with improved styling and functionality

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,24 +1,30 @@
-import styled from 'styled-components'
-import { categories } from "../data";
+// Categories.js
+import styled from 'styled-components';
+import { categories } from '../data';
 import CategoryItem from './CategoryItem';
-import {mobile} from '../responsive'
-
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     display: flex;
-    padding: 20px;
+    padding: 40px;
     justify-content: space-between;
-    ${mobile({flexDirection:"column;"})}
+    gap: 30px; /* Gap between items */
+    align-items: stretch; /* Ensure equal heights */
+    flex-wrap: wrap; /* Allow wrapping on smaller screens */
+    border-radius: 30px; /* Set border radius */
+    background-color: white; /* Add background color to see the border radius */
+    overflow: hidden; /* Ensure the border radius is visible by hiding overflow */
+    ${mobile({ flexDirection: 'column', gap: '10px' })}; /* Stack items vertically on mobile */
 `;
 
 const Categories = () => {
     return (
         <Container>
             {categories.map((item) => (
-                <CategoryItem item={item} key={item.id}/>
+                <CategoryItem item={item} key={item.id} />
             ))}
         </Container>
-    )
-}
+    );
+};
 
-export default Categories
+export default Categories;
