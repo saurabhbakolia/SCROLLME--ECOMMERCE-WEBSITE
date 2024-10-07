610e8f1b044d328eda9d81b561503f7ff5b6434f
samriddhitiwary <samriddhitiwary01@gmail.com>
2024-10-07 21:06:52 +0530

    Fixed Register Page styling

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,63 +1,17 @@
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+import { UserRegistrationAPI } from '../services/userAPI/registerationAPI';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { useDispatch } from "react-redux";
 import { changeAuthenticated } from "../store/Slices/UserSlice";
-import { useToast } from "@chakra-ui/react";
-import { auth } from "../context/Firebase"; // Update import
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth"; // Import user creation method
-import { FaGoogle } from 'react-icons/fa'; // Import Google icon
-import { UserRegistrationAPI } from '../services/userAPI/registerationAPI';
-import { useState } from 'react';
 import { Triangle } from 'react-loader-spinner';
 import { mobile, tablet } from '../responsive';
 import PasswordStrengthBar from 'react-password-strength-bar';
+import { useToast } from '@chakra-ui/react';
 
-
-const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: linear-gradient(
-        rgba(255, 255, 255, 0.5),
-        rgba(255, 255, 255, 0.5)
-    ),
-    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-    center;
-    background-size: cover;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
-const PasswordBox = styled.div`
-  width: 100%;
-  padding: 2px 4px;
-  height: fit-content;
-`;
-
-const Box = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  padding: 6px;
-`;
-
-const Text = styled.p`
-  font-size: 12px;
-`;
-
-const Link = styled.a`
-  font-size: 12px;
-  margin: 5px 0px;
-  text-decoration: underline;
-  cursor: pointer;
-`;
-
-const Agreement = styled.span`
-  font-size: 12px;
-  margin: 20px 0px;
-`;
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"; // Import user creation method
+import { auth } from "../context/Firebase"; // Update import
+import { FaGoogle } from 'react-icons/fa'; // Add this import
 
 const LoaderOverlay = styled.div`
   position: absolute;
@@ -72,105 +26,95 @@ const LoaderOverlay = styled.div`
   z-index: 10;
 `;
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background:
+    linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),
+    url('https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 6px;
+`;
+
 const Wrapper = styled.div`
-    width: 25%;
+  width: 40%;
+  ${mobile({ width: '84%;' })}
+  ${tablet({ width: '84%;' })}
+    ${mobile({ maxWidth: '760px' })}
     padding: 20px;
-    background-color: white;
+  background-color: white;
 `;
 
 const Title = styled.h1`
-    font-size: 24px;
-    font-weight: 300;
+  font-size: 24px;
+  font-weight: 300;
 `;
 
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const Input = styled.input`
-    flex: 1;
-    min-width: 40%;
-    margin: 10px 0;
-    padding: 10px;
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Text = styled.p`
+  font-size: 12px;
+`;
+
+const Link = styled.a`
+  font-size: 12px;
+  margin: 5px 0px;
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 const Button = styled.button`
-    width: 40%;
-    border: none;
-    padding: 15px 20px;
-    background-color: teal;
-    color: white;
-    cursor: pointer;
-    margin-bottom: 10px;
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
 `;
 
+const PasswordBox = styled.div`
+  width: 100%;
+  padding: 2px 4px;
+  height: fit-content;
+`;
 const GoogleButton = styled(Button)`
     background-color: #4285f4; /* Google blue color */
-    width: 100%; /* Full width */
+    width: 40%; /* Full width */
 `;
-
 const Register = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const toast = useToast();
-    const googleProvider = new GoogleAuthProvider();
-    const [isLoading, setIsLoading] = useState(false);
-    const [password, setPassword] = useState('');
-
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
-            toast({
-                title: "Registration Successful",
-                description: "You have successfully registered.",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-            });
-            dispatch(changeAuthenticated(true));
-            navigate("/");
-        } catch (error) {
-            toast({
-                title: "Registration Failed!",
-                description: error.message || "Please try again.",
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-            console.log(error);
-        }
-    };
-
-    const handleGoogleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-            const user = result.user;
-            toast({
-                title: "Google Login Successful",
-                description: `Welcome ${user.displayName}!`, // Corrected syntax
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-            });
-            dispatch(changeAuthenticated(true));
-            navigate("/");
-        } catch (error) {
-            toast({
-                title: "Google Login Failed!",
-                description: "Please try again later.",
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-            console.log(error);
-        }
-    };
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const toast = useToast();
+  const dispatch = useDispatch();
+  const googleProvider = new GoogleAuthProvider();
 
   const validateForm = (formData) => {
     const { firstName, lastName, username, password } = formData;
@@ -231,6 +175,31 @@ const Register = () => {
 
     return true;
   };
+
+  const handleGoogleLogin = async () => {
+    try {
+        const result = await signInWithPopup(auth, googleProvider); // Use auth from Firebase context
+        const user = result.user;
+        toast({
+            title: "Google Registration Successful",
+            description: `Welcome ${user.displayName}!`,
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+        });
+        dispatch(changeAuthenticated(true));
+        navigate("/");
+    } catch (error) {
+        toast({
+            title: "Google Registration Failed!",
+            description: "Please try again later.",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+        });
+        console.log(error);
+    }
+};
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -353,8 +322,9 @@ const Register = () => {
             </Box>
             <Button type="submit">CREATE</Button>
           </Form>
+          <br></br>
           <GoogleButton onClick={handleGoogleLogin}>
-            <FaGoogle style={{ marginRight: '8px' }} /> Resiter With with Google
+            <FaGoogle style={{ marginRight: '8px' }} /> Sign in with Google
           </GoogleButton>
         </Wrapper>
       </Container>
