e22e3ff1e564e6bdccee6239d18750fa1e4dfb4d
AMAR <amarjha880@gmail.com>
2024-10-13 15:00:45 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,29 @@
-
-import Announcement from '../components/Announcement';
-import Categories from '../components/Categories';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-import Newsletter from '../components/Newsletter';
-import Products from '../components/Products';
-import Slider from '../components/Slider';
-import FAQ from '../components/FAQ';
-
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
+import FAQ from "../components/FAQ";
+import BackToTopButton from '../components/BackToTopButton';
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
       <Slider />
+      <BackToTopButton/>
       <Categories />
       <Products />
       <FAQ />
       <Newsletter />
+     
       <Footer />
     </div>
   );
 };
 
 export default Home;
+
