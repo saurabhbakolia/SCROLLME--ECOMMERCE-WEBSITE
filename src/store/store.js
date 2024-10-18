038997ee39dffeff56e65a5628738f750c0bbd5b
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2024-10-18 23:42:58 +0530

    feat: add wishlist feature with heart icon toggle functionality

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, 
 import storage from 'redux-persist/lib/storage';
 import userReducer from './slices/userSlice';
 import cartReducer from './slices/cartSlice';
+import wishlistReducer from './slices/wishlistSlice';
 
 // persist configuration
 const persistConfig = {
@@ -14,11 +15,13 @@ const persistConfig = {
 // Create a persisted reducer
 const persistedReducer = persistReducer(persistConfig, userReducer);
 const persistedCartReducer = persistReducer({ key: 'cart', storage }, cartReducer);
+const persistedWishlistReducer = persistReducer({ key: 'wishlist', storage }, wishlistReducer);
 
 export const store = configureStore({
   reducer: {
     user: persistedReducer,
     cart: persistedCartReducer,
+    wishlist: persistedWishlistReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
