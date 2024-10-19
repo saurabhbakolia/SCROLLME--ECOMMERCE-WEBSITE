644ff87c9dcc7dff4fc9dc5ee736fd3af36bea3e
saras-69 <115482884+saras-69@users.noreply.github.com>
2024-10-20 01:10:27 +0530

    Password-recovery feature

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ProtectedRoute from './utils/ProtectedRoute';
 import ContributorsPage from './pages/ContributorsPage';
 import { lazy, Suspense } from 'react';
 import ContactUsPage from './pages/ContactUsPage';
+import ForgotPassword from './pages/ForgotPassword';  
 const LazyAllProducts = lazy(() => import('./pages/AllProducts'));
 const LazyCategoryProducts = lazy(() => import('./pages/CategoryProducts'));
 
@@ -44,6 +45,7 @@ function App() {
         <Route path='/contributors' element={<ContributorsPage />} />
         <Route path='/:categorySlug/:productId' element={<Product />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} /> {/* Add route for ForgotPassword */}
         <Route path='/register' element={<Register />} />
         <Route path='/contact-us' element={<ContactUsPage />} />
         <Route path='*' element={<h1>404 Not Found</h1>} />
