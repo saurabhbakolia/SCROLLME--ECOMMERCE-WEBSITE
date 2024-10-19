3a7acec6b039e46399f493bb1a4e89468bd06de4
SIDDHARTH A <90645484+Siddhart2004@users.noreply.github.com>
2024-10-19 13:32:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,22 @@ import styled from 'styled-components';
 import SearchIcon from '@mui/icons-material/Search';
 import { ShoppingCartOutlined } from '@mui/icons-material';
 import { mobile } from '../responsive';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom'; // Added useLocation
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, useToast } from '@chakra-ui/react';
+import { Box, useToast, Badge } from '@chakra-ui/react';
 import { AUTH_ENDPOINTS } from '../api/endPoints';
 import axios from 'axios';
 import { logOut } from '../store/slices/userSlice';
-import { Badge } from '@chakra-ui/react';
 import Logo from '../components/Logo';
 
-const Container = styled.div`
+const Container = styled.div
   height: 60px;
   width: 100%;
   background-color: white;
   ${mobile({ height: '120px;' })}
-`;
+;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
@@ -30,65 +29,76 @@ const Wrapper = styled.div`
     justifyContent: 'space-evenly',
     alignItems: 'center',
   })}
-`;
+;
 
-const Left = styled.div`
+const Left = styled.div
   flex: 1;
   display: flex;
   align-items: center;
-`;
+;
 
-const Language = styled.span`
+const Language = styled.span
   font-size: 14px;
   cursor: pointer;
   ${mobile({ display: 'none;' })}
-`;
+;
 
-const SearchContainer = styled.div`
+const SearchContainer = styled.div
   border: 1px solid lightgray;
   display: flex;
   align-items: center;
   margin-inline-start: 25px;
   padding: 5px;
-`;
+;
 
-const Input = styled.input`
+const Input = styled.input
   border: none;
-  margin: 'auto' ${mobile({ width: '200px;', margin: '0 auto' })};
-`;
-const Center = styled.div`
+  ${mobile({ width: '200px', margin: '0 auto' })}
+;
+
+const Center = styled.div
   flex: 1;
   text-align: center;
-`;
+;
 
-const Right = styled.div`
+const Right = styled.div
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: center;
   ${mobile({ flex: 2, justifyContent: 'center' })}
-`;
+;
 
-const MenuItem = styled.div`
+// Active and hover styling
+const MenuItem = styled(Link)
   font-size: 14px;
   cursor: pointer;
   margin-inline-start: 25px;
+  text-decoration: none;
+  color: ${(props) => (props.active ? 'teal' : 'black')};
+  font-weight: ${(props) => (props.active ? 'bold' : 'normal')};
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: teal;
+  }
+
   ${mobile({ fontSize: '12px', marginInlineStart: '10px' })}
-`;
+;
 
 const Navbar = () => {
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const cartItems = useSelector((state) => state.cart.items);
   const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+
   const navigate = useNavigate();
+  const location = useLocation(); // Hook to get current path
   const dispatch = useDispatch();
   const toast = useToast();
 
   const handleLogout = async () => {
     try {
-      const res = await axios.post(`${AUTH_ENDPOINTS.LOGOUT}`, {
-        withCredentials: true, // Include cookies in the request
-      });
+      const res = await axios.post(${AUTH_ENDPOINTS.LOGOUT}, { withCredentials: true });
 
       if (res.status === 200) {
         toast({
@@ -113,6 +123,9 @@ const Navbar = () => {
     }
   };
 
+  // Function to check if a route is active
+  const isActive = (path) => location.pathname === path;
+
   return (
     <Container>
       <Wrapper>
@@ -125,36 +138,37 @@ const Navbar = () => {
         </Left>
         <Center>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold' }}>
-            {' '}
-            <Logo />{' '}
+            <Logo />
           </div>
         </Center>
         <Right>
-          <MenuItem>
-            <Link to='/contact-us'>CONTACT US</Link>
+          <MenuItem to='/contact-us' active={isActive('/contact-us')}>
+            CONTACT US
           </MenuItem>
           {!isAuthenticated && (
-            <MenuItem>
-              <Link to='/register'>REGISTER</Link>
+            <MenuItem to='/register' active={isActive('/register')}>
+              REGISTER
             </MenuItem>
           )}
           {!isAuthenticated && (
-            <MenuItem>
-              <Link to='/login'>SIGN IN</Link>
+            <MenuItem to='/login' active={isActive('/login')}>
+              SIGN IN
             </MenuItem>
           )}
-          {isAuthenticated && <MenuItem onClick={handleLogout}>LOG OUT</MenuItem>}
-          <MenuItem>
-            <Link to='/cart'>
-              <Box position='relative'>
-                <ShoppingCartOutlined />
-                {totalQuantity > 0 && (
-                  <Badge colorScheme='teal' borderRadius='full' position='absolute' top='-5px' right='-10px' fontSize='0.8em'>
-                    {totalQuantity}
-                  </Badge>
-                )}
-              </Box>
-            </Link>
+          {isAuthenticated && (
+            <MenuItem as='div' onClick={handleLogout}>
+              LOG OUT
+            </MenuItem>
+          )}
+          <MenuItem to='/cart' active={isActive('/cart')}>
+            <Box position='relative'>
+              <ShoppingCartOutlined />
+              {totalQuantity > 0 && (
+                <Badge colorScheme='teal' borderRadius='full' position='absolute' top='-5px' right='-10px' fontSize='0.8em'>
+                  {totalQuantity}
+                </Badge>
+              )}
+            </Box>
           </MenuItem>
         </Right>
       </Wrapper>
