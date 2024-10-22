146d80ac1cff000f2ba6e317e323cf2d14193d81
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2024-10-22 10:29:23 +0530

    fix;

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ import ProtectedRoute from './utils/ProtectedRoute';
 import ContributorsPage from './pages/ContributorsPage';
 import { lazy, Suspense } from 'react';
 import ContactUsPage from './pages/ContactUsPage';
-import ForgotPassword from './pages/ForgotPassword'; // Import ForgotPassword component
-import useAuthCheck from './hooks/useAuthCheck'; // Import useAuthCheck hook
-
+import ForgotPassword from './pages/ForgotPassword';
+import useAuthCheck from './hooks/useAuthCheck';
 const LazyAllProducts = lazy(() => import('./pages/AllProducts'));
 const LazyCategoryProducts = lazy(() => import('./pages/CategoryProducts'));
 
@@ -57,7 +56,7 @@ function App() {
         <Route path='/contributors' element={<ContributorsPage />} />
         <Route path='/:categorySlug/:productId' element={<Product />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} /> {/* Add route for ForgotPassword */}
+        <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/register' element={<Register />} />
         <Route path='/contact-us' element={<ContactUsPage />} />
         <Route path='*' element={<h1>404 Not Found</h1>} />
